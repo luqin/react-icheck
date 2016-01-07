091c1869d0210356964a57e772fedf982ab15c22
luqin <h-io@qq.com>
2016-01-07 22:54:29 +0800

    update dependencies

--- examples/src/js/app.js
+++ examples/src/js/app.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { IndexRoute, Router, Route } from 'react-router';
-import createHistory from 'history/lib/createHashHistory';
-
-const history = createHistory({ queryKey: false });
+import { hashHistory, IndexRoute, Router, Route } from 'react-router';
 
 import App from './components/App';
-import GettingStarted from './components/GettingStarted';
+import GettingStarted from 'react-router!./components/GettingStarted';
 import PageNotFound from './components/PageNotFound';
-import Basic from './components/examples/Basic';
+import Basic from 'react-router!./components/examples/Basic';
 
 const routes = (
-  <Router history={ history }>
+  <Router history={ hashHistory }>
     <Route path="/" component={App}>
       <IndexRoute component={Basic}/>
       <Route path="getting-started" component={GettingStarted}/>
